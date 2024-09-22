fd0c1254d73f949972e566c44115cb42a2e142ec
Mateusz <mateusz4k@outlook.com>
2024-09-22 20:43:33 +0200

    feat: set values and handle errors.

--- src/Command/AddCommand.php
+++ src/Command/AddCommand.php
@@ -3,7 +3,9 @@
 namespace App\Command;
 
 
-use Symfony\Component\Console\Command\Command;
+use App\Services\ExpensesStorage;
+
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,21 +16,24 @@ use Symfony\Component\Console\Output\ConsoleOutputInterface;
 #[AsCommand(
     name: 'add',
     description: "Add expenses.",
-    aliases: ['add', 'a'],
+    aliases: ['a'],
     hidden: false,
 )]
-class AddCommand extends Command
+class AddCommand extends SymfonyCommand
 {
     private const DESCRIPTION = "description";
-    private const SHORT_DESRIPTION = 'desc';
+    private const SHORT_DESRIPTION = 'd';
     private const AMOUNT = "amount";
     private const SHORT_AMOUNT = 'a';
+    private const CATEGORY = 'category';
+    private const SHORT_CATEGORY = 'c';
 
     protected function configure()
     {
         $this
             ->addOption(self::DESCRIPTION, self::SHORT_DESRIPTION, InputOption::VALUE_REQUIRED, "Description of the expense.")
-            ->addOption(self::AMOUNT, self::SHORT_AMOUNT, InputOption::VALUE_REQUIRED, "Amount of the expense.");
+            ->addOption(self::AMOUNT, self::SHORT_AMOUNT, InputOption::VALUE_REQUIRED, "Amount of the expense.")
+            ->addOption(self::CATEGORY, self::SHORT_CATEGORY, InputOption::VALUE_REQUIRED, "Category of the expense.");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -36,22 +41,32 @@ class AddCommand extends Command
         if (!$output instanceof ConsoleOutputInterface) {
             throw new \LogicException("This command accepts only an instance of 'ConsoleOutputInterface'.");
         }
-        $description = $this->validateInput($input->getoption(self::DESCRIPTION), self::DESCRIPTION);
-        $amount = $this->validateInput($input->getOption(self::AMOUNT), self::AMOUNT);
 
-        echo "$description, $amount";
 
-        return Command::SUCCESS;
+        $data = [
+            "description" => $this->validateInput($input->getoption(self::DESCRIPTION), self::DESCRIPTION),
+            "amount" => $this->validateInput($input->getOption(self::AMOUNT), self::AMOUNT, true),
+            "category" => $this->validateInput($input->getOption(self::CATEGORY), self::CATEGORY)
+        ];
+
+        $storage = new ExpensesStorage();
+        $storage->addNewExpense($data);
+
+        return SymfonyCommand::SUCCESS;
     }
 
-    private function validateInput(string | null  $input, string $name)
+    private function validateInput(string | null  $input, string $name, bool $isNumeric = false)
     {
         if (!$input || empty(trim($input))) {
-            echo "\e[31m".ucfirst($name) . " is required.\e[0m";
+            echo "\e[31m" . ucfirst($name) . " is required.\e[0m";
             exit;
         }
-        if(is_numeric($input) && (int) $input < 0){
-            echo "\e[31m".ucfirst($name) . " Can't be negative.\e[0m";
+        if ($isNumeric && !is_numeric($input)) {
+            echo "\e[31m" . ucfirst($name) . " must be an integer.";
+            exit;
+        }
+        if ($isNumeric && (int) $input < 0) {
+            echo "\e[31m" . ucfirst($name) . " Can't be negative.\e[0m";
             exit;
         }
         return $input;
