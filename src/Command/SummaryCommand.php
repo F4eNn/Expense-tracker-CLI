14d6f8b25441c2ac03eff978ab7f17775db8404d
Mateusz <mateusz4k@outlook.com>
2024-09-22 20:43:53 +0200

    feat: created sumary command

---
+++ src/Command/SummaryCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use App\Services\ExpensesStorage;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'summary',
+    hidden: false,
+    description: "Total expenses."
+)]
+class SummaryCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->addOption("month", "m", InputOption::VALUE_OPTIONAL, "Total expenses for selected month.");
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $storage = new ExpensesStorage();
+        $month = $input->getOption("month");
+
+        if ($month) {
+            if (!is_numeric($month)) {
+                $output->writeln("<error>Month must be an integer.</error>");
+                return Command::FAILURE;
+            }
+            if ($month < 1 || $month > 12) {
+                $output->writeln("<error>Invalid month.</error>");
+                return Command::FAILURE;
+            }
+        }
+
+        $summary  = $storage->summaryExpenses($month);
+        $output->writeln("<info>$summary</info>");
+
+        return Command::SUCCESS;
+    }
+}
